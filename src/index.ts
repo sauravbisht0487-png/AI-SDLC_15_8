1769eb4b348d3b1a234a3f1d452f99ac40a7a045
Chandra Bhanu Bisht <sauravbisht0487@gmail.com>
2026-09-16 15:23:14 +0530

    test: intentional CI failure

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,6 @@ const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 
-
-
-
 import authRoutes from "./routes/authRoutes";
 import organizationRoutes from "./routes/organizationRoutes";
 
@@ -23,7 +20,7 @@ const allowedOrigins = [
   'https://ai-sdlc-frontend-one.vercel.app',
   'https://ai-sdlc-frontend.vercel.app', // your real stable domain, once you confirm it
 ];
-
+   const brokenVar: number = "this is a string";
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
